d343aa1f8c742a98c32fec68acc8a63ac152e3cd
Петренков Николай <@corp.itstep>
2018-01-25 10:01:41 +0600

    My gas station

---
+++ Food.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GasStation
+
+{
+    public class Food : INotifyPropertyChanged
+    {
+        private int id;
+        private double price;
+        private int count;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+                OnPropertyChanged("Count");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GasStation
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Food gas;
+        private Food burger;
+        private Food drink;
+        private Food chocolate;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            gas = new Food();
+            burger = new Food();
+            drink = new Food();
+            chocolate = new Food();
+
+            textboxGasPrice.DataContext = gas;
+            textboxGasCount.DataContext = gas;
+            textboxBurgersPrice.DataContext = burger;
+            textboxBurgersCount.DataContext = burger;
+            textboxDrinkPrice.DataContext = drink;
+            textboxDrinkCount.DataContext = drink;
+            textboxChocolatePrice.DataContext = chocolate;
+            textboxChocolateCount.DataContext = chocolate;
+        }
+
+        private void buttonClick(object sender, RoutedEventArgs e)
+        {
+            textboxResult.Text = gas.Price * gas.Count + burger.Price * burger.Count + drink.Price * drink.Count + chocolate.Price * chocolate.Count + "";
+        }
+    }
+}
